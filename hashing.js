9ee570c5a1939f581f6ec052e5ae07e88e8c53a5
Bryant Vail <Bryanttv@outlook.com>
2018-06-05 17:21:05 -0400

    end hash_1

--- hashing.js
+++ hashing.js
@@ -9,3 +9,10 @@
     ]
     
 */
+
+function HashTable(){
+    this.table =        new Array(137);
+    this.simpleHash =   simpleHash;
+    this.showDistro =   showDistro;
+    this.put =          put;
+}
